dddaf960b5bedb0d116c7179a4a64714ab171fbe
zotian <zotiano@hotmail.com>
2021-01-24 03:44:59 +0200

    eslint fixes

--- vue_lottery_app/src/main.js
+++ vue_lottery_app/src/main.js
@@ -1,16 +1,16 @@
-import '@/scss/main.scss'
+import "@/scss/main.scss";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import * as store from './store'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import * as store from "./store";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 new Vue({
   router,
   store: store.default,
--- vue_lottery_app/src/store/modules/login.js
+++ vue_lottery_app/src/store/modules/login.js
@@ -1,41 +1,79 @@
-import axios from 'axios'
-const loginPath = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_LOGIN_KEY}`
-const registerPath = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_LOGIN_KEY}`
+import axios from "axios";
+const loginPath = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_LOGIN_KEY}`;
+const registerPath = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_LOGIN_KEY}`;
 const state = {
   userData: []
-}
-const getters = {}
+};
+const getters = {};
 const mutations = {
-  UPDATE_USER_DATA (state, payload) {
-    const {res} = payload
-    state.userData = payload
-    localStorage.setItem('loginData', JSON.stringify(res.data))
+  UPDATE_USER_DATA(state, payload) {
+    const { res } = payload;
+    state.userData = payload;
+    localStorage.setItem("loginData", JSON.stringify(res.data));
   }
-}
+  // LOGOUT (payload) {
+  //   const {vm} = payload
+  //   localStorage.removeItem('loginData');
+  //   vm.$bvToast.toast('Session Expired', {
+  //     title: 'Error',
+  //     variant: 'danger',
+  //     solid: true
+  //     })
+  // }
+};
 const actions = {
-  loginRegister({commit}, payload) {
-    const {vm, formData, returnSecureToken, action} = payload
+  loginRegister({ commit }, payload) {
+    const { vm, formData, returnSecureToken, action } = payload;
     const requestBody = {
       ...formData,
       returnSecureToken
-    }
-    const path = action === 'login' ? loginPath : registerPath
+    };
+    const path = action === "login" ? loginPath : registerPath;
     return new Promise((resolve, reject) => {
       axios
         .post(path, requestBody)
-          .then(res => {
-            commit('UPDATE_USER_DATA', {
-              res,
-              vm
-            })
-            resolve(res)
-          })
-          .catch((err) => {
-            reject(err)
-          })
-    })
+        .then(res => {
+          commit("UPDATE_USER_DATA", {
+            res,
+            vm
+          });
+          resolve(res);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   }
-}
+};
+// const checkExpiredToken = (commit) => {
+//   const refreshToken = JSON.parse(localStorage.getItem('loginData')).refreshToken
+//   const idToken = SON.parse(localStorage.getItem('loginData')).idToken
+//   if (!idToken) {
+//     commit('LOGOUT', {vm:this})
+//   } else {
+//     const expirationTime = JSON.parse(localStorage.getItem('loginData')).expiresIn
+//     const expirationDate = new Date(expirationTime)
+//     if (expirationDate <= new Date()) {
+//       commit('LOGOUT')
+//     } else {
+//       checkAuthTimeout(expirationDate)
+//     }
+//   }
+// }
+// const checkAuthTimeout = (expirationDate) => {
+//   console.log(expirationDate)
+//   const requestPayload = {'refresh_token': refreshToken, 'grant_type': 'refresh_token'}
+//   setTimeout(() => {
+//     axios
+//       .post(`https://securetoken.googleapis.com/v1/token?key=${process.env.VUE_APP_LOGIN_KEY}`, requestPayload)
+//         .then((res) => {
+//           localStorage.setItem('loginData', JSON.stringify(res.data))
+//         })
+//         .catch(err => {
+//           console.log(err)
+//         })
+//   }, 10000)
+// }
 
 export default {
   namespaced: true,
@@ -43,4 +81,4 @@ export default {
   getters,
   mutations,
   actions
-}
+};
