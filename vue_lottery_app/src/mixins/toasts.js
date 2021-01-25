983b3b52796eabf441ca8281c03179d02cf7fddb
zotian <zotiano@hotmail.com>
2021-01-25 16:47:29 +0200

    Feature: add more toast options in mixin

--- vue_lottery_app/src/mixins/toasts.js
+++ vue_lottery_app/src/mixins/toasts.js
@@ -1,12 +1,27 @@
-export default {
+const Toasts = {
   methods: {
-    toast (payload) {
-      const {body, title, variant} = payload
+    customToast(payload) {
+      const { body, title, variant } = payload;
       this.$bvToast.toast(body, {
         title,
         variant,
         solid: true
-        })
+      });
+    },
+    successToast(textBody) {
+      this.$bvToast.toast(textBody, {
+        title: "Success",
+        variant: "success",
+        solid: true
+      });
+    },
+    errorToast(textBody) {
+      this.$bvToast.toast(textBody, {
+        title: "Error",
+        variant: "danger",
+        solid: true
+      });
     }
   }
-}
+};
+export default Toasts;
