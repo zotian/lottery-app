2729921220e190f37194810a05755b8bed4e8264
zotian <zotiano@hotmail.com>
2021-01-25 16:48:02 +0200

    eslint fixes

--- vue_lottery_app/src/common-js/timeConverts.js
+++ vue_lottery_app/src/common-js/timeConverts.js
@@ -1,9 +1,16 @@
-const convertTimeStamp = (timeStamp) => {
-    var date = new Date(timeStamp);
-    var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear() + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
-    return formattedDate
-}
+const convertTimeStamp = timeStamp => {
+  var date = new Date(timeStamp);
+  var formattedDate =
+    ("0" + date.getDate()).slice(-2) +
+    "/" +
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    "/" +
+    date.getFullYear() +
+    " " +
+    ("0" + date.getHours()).slice(-2) +
+    ":" +
+    ("0" + date.getMinutes()).slice(-2);
+  return formattedDate;
+};
 
-export {
-    convertTimeStamp
-}
+export { convertTimeStamp };
